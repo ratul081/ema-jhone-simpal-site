a195c187c360e3bd6688f5fb56a424c6ecd60406
ratul081 <ratullinux77@gmail.com>
2023-02-10 12:31:25 +0600

    Cart done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+const Header = () => {
+  return (
+    <nav className='header'>
+			<img src={logo} alt=""></img>
+			<div>
+				<a href="./order">Order</a>
+				<a href="./order-review">Order Review</a>
+				<a href="./manage-inventory">Manage Inventory</a>
+				<a href="./login">Login</a>
+			</div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css'
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+
+
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [])
+  const handleAddToCart = (product) => {
+    console.log(product)
+    // cart.push(product)
+    const newCart = [...cart, product];
+    console.log(newCart)
+    setCart(newCart);
+  }
+  return (
+    <div className='shop'>
+      <div className="products-container">
+        {
+          products.map(product => <Product
+            product={product}
+            handleAddToCart={handleAddToCart}
+            key={product.id}
+          ></Product>)
+        }
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
